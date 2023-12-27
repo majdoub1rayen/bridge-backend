b7721674505fb91cc9c4640d63c999f242c2f23a
majdoub1rayen <139793543+majdoub1rayen@users.noreply.github.com>
2023-12-27 13:36:59 +0100

    initial-commit

---
+++ src/test/java/com/bridge/backoffice/BackOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridge.backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
